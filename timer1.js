1e4780cea5e79a1a7fe18ba939763a8ff04fcbb8
hseiji <henrique.hst@gmail.com>
2022-04-16 09:46:49 -0400

    fix the timer

--- timer1.js
+++ timer1.js
@@ -1,13 +1,19 @@
 const args = process.argv.slice(2);
 
 let timer = 0;
+
 for (const item of args) {
-  const number = Number(item);
   // It should be a number, positive and a number should be provided
-  if (item !== "" && number !== NaN && item > 0) {
-    timer = timer + item * 1000;
-    setTimeout(() => {
-      process.stdout.write('.');
-    }, timer);
+  
+  number = parseInt(item);
+  // It verifies if the argument is a number
+  if (number != item) {
+    console.log("Please insert a valid number (it must be a positive number).");
+    return;
   }
+
+  setTimeout(() => {
+    process.stdout.write('.');
+  }, number * 1000);
+  
 }
